bce8d8bb3e1304d8c3dadcb30c5ba7a08e5151ad
Karam Yaaqba <kyaaqba@hotmail.com>
2024-12-29 09:55:25 +0200

    add site

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+    { path: '', component: AppComponent }, 
+  ];
+  
